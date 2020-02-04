e01493b93e62e905a3adf89f79388891f073f844
Alexander Bachschmid <alexander.bachschmid@prochannel.eu>
2020-02-04 01:29:25 +0100

    vueJS

---
+++ vuejs/new-work-finance/src/components/Loading.vue
@@ -0,0 +1,6 @@
+<template>
+  <!-- loading start -->
+  <div class="loading">
+    <img class="logo-loading" src="assets/images/logo/logo-coha-w-2z.png" alt="logo">
+  </div><!-- loading end -->
+</template>
